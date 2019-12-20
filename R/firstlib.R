8f792d40dc47d39deb423fa6077c5e4135ef0d78
Leo Lahti <leo.lahti@iki.fi>
2019-12-20 10:00:02 +0000

    version 3.4.20002

--- R/firstlib.R
+++ R/firstlib.R
@@ -11,6 +11,7 @@
 #' @importFrom httr build_url
 #' @importFrom httr content
 #' @importFrom httr GET
+#' @importFrom httr http_error
 #' @importFrom httr status_code
 #' @importFrom jsonlite fromJSON
 #' @importFrom methods as
